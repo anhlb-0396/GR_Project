6ccbce42c6ea5c827b2ccb5ef89b4ce86bebe7b1
anhlb-0396 <le.bao.anh@sun-asterisk.com>
2024-06-29 11:18:55 +0700

    IP Addr change FE

--- GR_FE/src/constants/urlConstants.js
+++ GR_FE/src/constants/urlConstants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:3001/api";
-export const SOCKET_SERVER_URL = "http://localhost:3001";
+export const BASE_URL = "http://20.244.91.45:3001/api";
+export const SOCKET_SERVER_URL = "http://20.244.91.45:3001";
