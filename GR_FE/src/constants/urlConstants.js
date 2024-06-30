fe64b286ce46a12b1750910d388f5bbf3a7eb59d
anhlb-0396 <le.bao.anh@sun-asterisk.com>
2024-06-30 19:28:00 +0700

    Push into product

--- GR_FE/src/constants/urlConstants.js
+++ GR_FE/src/constants/urlConstants.js
@@ -1,2 +1,5 @@
-export const BASE_URL = "http://localhost:3001/api";
-export const SOCKET_SERVER_URL = "http://localhost:3001";
+const deployedIp = "20.244.91.45";
+const localIp = "locahost";
+
+export const BASE_URL = `http://${deployedIp}:3001/api`;
+export const SOCKET_SERVER_URL = `http://${deployedIp}:3001`;
